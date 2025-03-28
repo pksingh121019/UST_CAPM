21474b6eb3d5ff035e4ac7cf521b201e2877e599
PRASHANT KUMAR <prashant.kumar4@ust.com>
2025-03-28 16:22:51 +0000

    first working version with MTA ACHICES

--- srv/catalougeService.js
+++ srv/catalougeService.js
@@ -1,69 +1,62 @@
-module.exports = cds.service.impl( async function(){
+const cds = require('@sap/cds');
 
-  //Step 1: get the object of our odata entities
-  const { EmployeeSet, POs } = this.entities;
+module.exports = cds.service.impl(async function () {
+    // Step 1: Get the object of our OData entities
+    const { EmployeeSet, POs } = this.entities;
 
-//Action -when,what,which 
-//when- on,before,after
-//example--no emp can have salary 1-lakh rs.
-      this.before('UPDATE', EmployeeSet,(req) => {
+    // ✅ Handle Root URL `/`
+    this.on('GET', '/', (req) => {
+        return 'Welcome to myfirstapp! OData service is available at <a href="/odata/v4/CatalogueService">/odata/v4/CatalogueService</a>';
+    });
 
+    // Validation before UPDATE
+    this.before('UPDATE', EmployeeSet, (req) => {
         var salary = parseInt(req.data.salaryAmount);
         if (salary >= 100000) {
-            req.error(500,'sorry invalid salary');
+            req.error(500, 'Sorry, invalid salary');
         }
-      }
+    });
 
-      );
-      this.after( 'READ',EmployeeSet,(data) => {
-          data.push({
+    // Add default employee data after READ
+    this.after('READ', EmployeeSet, (data) => {
+        data.push({
             "ID": "AB001",
-            "nmeFisrt" : "Munna"
-          });
-      }
+            "nmeFisrt": "Munna"
+        });
+    });
 
-      );
- 
-  this.on('boost', async (req,res) => {
-      try {
-        //get the PO key since this is  instance based action
-          const ID = req.params[0];
-          //just print the console for our understanding
-          console.log("Hey Amigo, Your purchase order with id " + req.params[0] + " will be boosted");
-         //start a db transaction using CDS ql
-          const tx = cds.tx(req);
-          //update the gross amount 
-          await tx.update(POs).with({
-              GROSS_AMOUNT: { '+=' : 20000 }
-              //NOTE: 'Boosted!!'
-          }).where(ID);
-      } catch (error) {
-          return "Error " + error.toString();
-      }
-  });
+    // Boost PO action
+    this.on('boost', async (req, res) => {
+        try {
+            const ID = req.params[0];
+            console.log("Hey Amigo, Your purchase order with id " + ID + " will be boosted");
+            const tx = cds.tx(req);
+            await tx.update(POs).with({
+                GROSS_AMOUNT: { '+=': 20000 }
+            }).where(ID);
+        } catch (error) {
+            return "Error " + error.toString();
+        }
+    });
 
-  this.on('getOrderDefaults', async (req,res) => {
-     return {
-        "OVERALL_STATUS": 'N',
+    // Get order defaults action
+    this.on('getOrderDefaults', async (req, res) => {
+        return {
+            "OVERALL_STATUS": 'N',
+        };
+    });
 
-     }
+    // Find the largest order
+    this.on('largestOrder', async (req, res) => {
+        try {
+            const tx = cds.tx(req);
+            const reply = await tx.read(POs).orderBy({
+                GROSS_AMOUNT: 'desc'
+            }).limit(5);
 
-  });
-  this.on('largestOrder', async (req,res) => {
-    try {
-        //const ID = req.params[0];
-        const tx = cds.tx(req);
-        
-        //SELECT * UPTO 1 ROW FROM dbtab ORDER BY GROSS_AMOUNT desc
-        const reply = await tx.read(POs).orderBy({
-            GROSS_AMOUNT: 'desc'
-        }).limit(5);
-
-        return reply;
-    } catch (error) {
-        return "Error " + error.toString();
-    }
+            return reply;
+        } catch (error) {
+            return "Error " + error.toString();
+        }
+    });
 });
-
-}
-);
